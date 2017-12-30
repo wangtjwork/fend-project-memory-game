65f60c52a39887e9ce41f3d6c07e93fed6a50cd8
Tianjian Wang <wangtjwork@gmail.com>
2017-12-30 14:32:10 -0600

    Construct random array

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 /*
  * Create a list that holds all of your cards
  */
-
-
+const cardArray = ['fa-anchor','fa-anchor','fa-bicycle','fa-bicycle','fa-bolt','fa-bolt',
+  'fa-bomb','fa-bomb','fa-cube','fa-cube','fa-diamond','fa-diamond','fa-leaf','fa-leaf',
+  'fa-paper-plane-o', 'fa-paper-plane-o'];
+shuffle(cardArray);
+console.log(cardArray);
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -12,7 +15,7 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
